88db317b36d1b5921e7703a2c008f6d1b8ba0465
Justin Acker <justin@justinacker.com>
2017-01-05 22:40:25 +0000

    refactor Issue class to accept hashes instead of variables, built out #add_issue_attributes and #self.create_from_collection

--- lib/cli.rb
+++ lib/cli.rb
@@ -7,9 +7,13 @@ class CommandLineInterface
 
   def make_issues
     issues_array = Scraper.scrape_release_page
+    add_attributes_to_issues
   end
 
   def display_issues
+  end
+
+  def add_attributes_to_issues
   end
 
 end
--- lib/issue.rb
+++ lib/issue.rb
@@ -4,24 +4,37 @@ class Issue
 
   @@all = []
 
-  def initialize(title, price, description, author, artist, publisher)
-    @title = title
-    @price = price
-    @description = description
-    @author = author
-    @artist = artist
-    @publisher = publisher
+  def initialize(issue_hash)
+
+    issue_hash.each do |key, value|
+      self.send("#{key}=", value)
+    end
+
     @@all << self
+
+    # @title = title
+    # @price = price
+    # @description = description
+    # @author = author
+    # @artist = artist
+    # @publisher = publisher
+
   end
 
   def  self.all
     @@all
   end
 
-  def self.create_from_collection
+  def self.create_from_collection(issues_array)
+    issues_array.each do |issue|
+      new_issue = Issue.new(issue)
+    end
   end
 
-  def add_issue_attributes
+  def add_issue_attributes(attributes_hash)
+    attributes_hash.each do |key, value|
+      self.send("{key=}", value)
+    end
   end
 
 end
