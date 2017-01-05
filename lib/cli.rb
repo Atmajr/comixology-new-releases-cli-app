bdf395afa86a66a061c2a807ea0e1c80dfc83cd3
Justin Acker <justin@justinacker.com>
2017-01-05 23:12:12 +0000

    implement CommandLineInterface#make_issues and #add_attributes_to_issues, add calls from #run

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,6 +3,8 @@ require_relative '../config/environment.rb'
 class CommandLineInterface
 
   def run
+    make_issues
+    add_attributes_to_issues
   end
 
   def make_issues
@@ -14,6 +16,10 @@ class CommandLineInterface
   end
 
   def add_attributes_to_issues
+    Issue.all.each do |issue|
+      attributes = Scraper.scrape_issue_page(issue.url)
+      issue.add_issue_attributes(attributes)
+    end
   end
 
 end
