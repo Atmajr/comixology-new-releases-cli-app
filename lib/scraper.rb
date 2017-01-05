310e0d5da5175f432cc4ea12886dd6732ded62e4
Justin Acker <justin@justinacker.com>
2017-01-05 00:50:10 +0000

    figuring out css for issue loop

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,10 +4,18 @@ require 'pry'
 
 class Scraper
 
-  def self.scrape_release_page(release_url)
+  def self.scrape_release_page(release_url = "https://www.comixology.com/new-comics")
+    #we'll default to the main new release page, but passing a variable instead of hard coding will make it easier to change if comixology
+    #changes their setup, and will also allow for expansion later
     page = Nokogiri::HTML(open(release_url))
     issues = []
     counter = 0
+    #binding.pry
+
+    page.css('.content-item').each do |item|
+      binding.pry
+    end
+
   end
 
   def self.scrape_issue_page(issue_url)
