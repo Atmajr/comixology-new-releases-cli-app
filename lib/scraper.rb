3301ecf04262cab73dd7ecdcb1a28d136f0e9e22
Justin Acker <justin@justinacker.com>
2017-01-05 00:36:51 +0000

    define scraper methods, initialize release page variables

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -3,4 +3,14 @@ require 'nokogiri'
 require 'pry'
 
 class Scraper
+
+  def self.scrape_release_page(release_url)
+    page = Nokogiri::HTML(open(release_url))
+    issues = []
+    counter = 0
+  end
+
+  def self.scrape_issue_page(issue_url)
+  end
+
 end
