49354e59a6e332dfa15202182ace7e9e22b54858
Justin Acker <justin@justinacker.com>
2017-01-06 00:30:19 +0000

    first build of #display_issue. not yet properly functional

--- lib/cli.rb
+++ lib/cli.rb
@@ -4,7 +4,7 @@ class CommandLineInterface
 
   def run
     make_issues
-    add_attributes_to_issues
+    #add_attributes_to_issues
     puts "Welcome to the Comixology New Release Extractor"
     puts "Please enter a number:"
     command = "0"
@@ -18,6 +18,7 @@ class CommandLineInterface
         when "1"
           list_issues
         when "2"
+          display_issue
         when "3"
         when "4"
           puts "Thank you for using the Comixology New Release Extractor!"
@@ -32,7 +33,29 @@ class CommandLineInterface
     Issue.create_from_collection(issues_array)
   end
 
-  def display_issues
+  def display_issue
+    total_releases = list_issues
+    input_is_valid = false
+    while !input_is_valid do
+      puts "Please enter the number of the issue you'd like to know more about:"
+      display_input = gets.chomp!.to_i
+      if display_input.between?(1,total_releases) then
+        chosen_issue = Issue.all[display_input - 1]
+        if chosen_issue.description = nil then
+          attributes_hash = Scraper.scrape_issue_page(chosen_issue.url)
+          chosen_issue.add_issue_attributes(attributes_hash)
+        end
+        puts "Title: #{chosen_issue.title}"
+        puts "Price: #{chosen_issue.price}"
+        puts "Description: #{chosen_issue.description}"
+        puts "Author: #{chosen_issue.author}"
+        puts "Artist: #{chosen_issue.artist}"
+        puts "Publisher: #{chosen_issue.publisher}"
+        input_is_valid = true
+      else
+        "Invalid input. Try again."
+      end
+    end
   end
 
   def add_attributes_to_issues
@@ -44,11 +67,14 @@ class CommandLineInterface
 
   def list_issues
     #binding.pry
+    number_of_releases = 0
     Issue.all.each do |issue|
+      number_of_releases++
       count = Issue.all.index(issue) + 1
       puts "#{count}.) #{issue.title} - #{issue.price}"
     end
     puts "------"
+    number_of_releases
   end
 
 end
