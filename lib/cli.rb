1110981955265ea44e9fb1940455bed92bc59ba5
Justin Acker <justin@justinacker.com>
2017-01-06 01:14:24 +0000

    finished build of #display_issue. functional.

--- lib/cli.rb
+++ lib/cli.rb
@@ -6,13 +6,12 @@ class CommandLineInterface
     make_issues
     #add_attributes_to_issues
     puts "Welcome to the Comixology New Release Extractor"
-    puts "Please enter a number:"
     command = "0"
-    while (command != "4") do
+    while (command != "3") do
       puts "1.) List new releases"
       puts "2.) Get more info about a release"
-      puts "3.) Display basic some basic help"
-      puts "4.) Exit this program"
+      puts "3.) Exit this program"
+      puts "Please enter a number:"
       command = gets.chomp!
       case command
         when "1"
@@ -20,7 +19,6 @@ class CommandLineInterface
         when "2"
           display_issue
         when "3"
-        when "4"
           puts "Thank you for using the Comixology New Release Extractor!"
         else
           puts "Invalid command. Please try again."
@@ -41,10 +39,11 @@ class CommandLineInterface
       display_input = gets.chomp!.to_i
       if display_input.between?(1,total_releases) then
         chosen_issue = Issue.all[display_input - 1]
-        if chosen_issue.description = nil then
+        if chosen_issue.description == nil then
           attributes_hash = Scraper.scrape_issue_page(chosen_issue.url)
           chosen_issue.add_issue_attributes(attributes_hash)
         end
+        binding.pry
         puts "Title: #{chosen_issue.title}"
         puts "Price: #{chosen_issue.price}"
         puts "Description: #{chosen_issue.description}"
@@ -69,7 +68,7 @@ class CommandLineInterface
     #binding.pry
     number_of_releases = 0
     Issue.all.each do |issue|
-      number_of_releases++
+      number_of_releases += 1
       count = Issue.all.index(issue) + 1
       puts "#{count}.) #{issue.title} - #{issue.price}"
     end
